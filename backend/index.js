d2906a744955304d4984eca41056aa27c963ae97
Ankit-1204 <ankit2003upadhyay@gmail.com>
2024-12-09 02:54:01 +0530

    add upload file function

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 const express=require('express')
 const cors =require('cors')
 const dotenv = require('dotenv');
-
+const multer =require('multer')
 // I could use this later.....
 // const corsOptions = {
 //     origin: 'http://localhost:3000',  
 //     methods: ['GET', 'POST'],        
 //     allowedHeaders: ['Content-Type'],
 //   };
-
+const uploads=multer({dest:'upload/'});
 dotenv.config()
 const app=express()
 app.use(cors())
@@ -23,5 +23,11 @@ router.get('/download/model/:name',(req,res)=>{
         res.status(404).json({ error: 'Model not found' });
     })
 })
-
+router.post('/upload',uploads.single('file'),(req,res)=>{
+    try {
+        res.status(200).json({message:'File Uploaded Successfully',file:req.file})
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
 app.listen(3000)
